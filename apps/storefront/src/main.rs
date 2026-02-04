e9f762194e4753b321b0be08fc42abae6d347b92
loid345 <volanar@tut.by>
2026-02-04 22:04:56 +0300

    Fix Leptos SSR render import

--- apps/storefront/src/main.rs
+++ apps/storefront/src/main.rs
@@ -5,8 +5,9 @@ use axum::{
     Router,
 };
 // GlobalAttributes enables id= usage in view! macros.
-use leptos::prelude::{ClassAttribute, CollectView, ElementChild, GlobalAttributes};
-use leptos::ssr::render_to_string;
+use leptos::prelude::{
+    render_to_string, ClassAttribute, CollectView, ElementChild, GlobalAttributes,
+};
 use leptos::{component, view, IntoView};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
