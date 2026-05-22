5e67b3023d66970ab3410d2a55cef6102d28ea35
loid345 <volanar@tut.by>
2026-05-23 00:24:00 +0300

    feat(mobile): add child page contracts to generated registry

--- rustok_mobile/packages/app_module_contracts/lib/app_module_contracts.dart
+++ rustok_mobile/packages/app_module_contracts/lib/app_module_contracts.dart
@@ -1,4 +1,5 @@
 library app_module_contracts;
 
+export 'src/mobile_child_page.dart';
 export 'src/mobile_module_entry.dart';
 export 'src/mobile_nav_meta.dart';
---
+++ rustok_mobile/packages/app_module_contracts/lib/src/mobile_child_page.dart
@@ -0,0 +1,11 @@
+class MobileChildPage {
+  const MobileChildPage({
+    required this.subpath,
+    required this.title,
+    this.navLabel,
+  });
+
+  final String subpath;
+  final String title;
+  final String? navLabel;
+}
